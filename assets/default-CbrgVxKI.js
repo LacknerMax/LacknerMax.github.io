22daefd5d3a22a0ce44aafdf6d13eaf423d88795
Maximilian Lackner <MLackner@schueler.hakzell.at>
2024-10-24 09:57:16 +0200

    upload

---
+++ assets/default-CbrgVxKI.js
@@ -0,0 +1,1 @@
+import{o,b as a,e as t,f as l}from"./modules/vue-B1fO7rse.js";import{u as r}from"./slidev/context-B08v-qlP.js";const d={class:"flex p-0 slidev-layout default"},n={class:"flex-1 p-8"},_={__name:"default",setup(i){return r(),(s,e)=>(o(),a("div",d,[e[0]||(e[0]=t("div",{class:"w-48 bg-gradient-to-tl gradient-primary"},null,-1)),t("div",n,[l(s.$slots,"default")])]))}};export{_};
